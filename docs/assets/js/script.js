cf3c5fc527d0bb5087eb5bc4cc4251f8d2bab88f
maashi <octtn8c@gmail.com>
2021-12-22 15:25:33 +0900

    build

---
+++ docs/assets/js/script.js
@@ -0,0 +1,96 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/_module/_changeImg.ts":
+/*!**************************************!*\
+  !*** ./src/js/_module/_changeImg.ts ***!
+  \**************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"changeImg\": function() { return /* binding */ changeImg; }\n/* harmony export */ });\nfunction changeImg() {\n    // 画像切り替え部分の要素を取得する\n    const imgGroup = document.querySelector('#js-group');\n    // 切り替え対象の要素の数を取得する\n    const img = imgGroup?.querySelectorAll('.js-img');\n    const count = Number(img?.length);\n    let index = 1;\n    // 3秒ごとにclassを切り替える\n    const timer = setInterval(() => {\n        // 一旦classをはずす\n        img?.forEach((element) => element.classList.remove('is-active'));\n        // 該当の要素のみclassをつける\n        img?.item(index).classList.add('is-active');\n        // 順番を回して要素の数になったらリセットする\n        if (index < count - 1) {\n            index++;\n        }\n        else {\n            index = 0;\n        }\n    }, 3000);\n}\n\n\n//# sourceURL=webpack://gulp-sample/./src/js/_module/_changeImg.ts?");
+
+/***/ }),
+
+/***/ "./src/js/assets/js/script.ts":
+/*!************************************!*\
+  !*** ./src/js/assets/js/script.ts ***!
+  \************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _module_changeImg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../_module/_changeImg */ \"./src/js/_module/_changeImg.ts\");\n\n(0,_module_changeImg__WEBPACK_IMPORTED_MODULE_0__.changeImg)();\n\n\n//# sourceURL=webpack://gulp-sample/./src/js/assets/js/script.ts?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/js/assets/js/script.ts");
+/******/ 	
+/******/ })()
+;
